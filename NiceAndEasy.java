d652e0d782aa2e2904aeeed7afbcd899a2f9f746
Maksim Stankevic <maksim.stankevic1@gmail.com>
2019-01-22 21:50:42 +0200

    beta

--- NiceAndEasy.java
+++ NiceAndEasy.java
@@ -17,10 +17,9 @@ public class NiceAndEasy {
 	public static final void main (String... args) {
 		
 		Stats theInstance = new Stats();
-		theInstance.enterCalcStore();
-		theInstance.printStats();
-
-		theInstance.serialize(theInstance);
+		theInstance.enterCalcStore(); //main method with infinite loop controlling console input, calculating ethyl alcohol quantity, storing data to DB
+		theInstance.printStats(); //output DB to console
+		theInstance.serialize(theInstance); //DB is implemented via object serialization
 	}
 }
 
@@ -48,56 +47,79 @@ class Stats implements Serializable {
 		}
 	}
 
-public void enterCalcStore () {
+	public void enterCalcStore () {
 
-                Console console = System.console();
-                if (console == null) throw new RuntimeException ("No console on this sytem, exiting!");
+                	Console console = System.console();
+                	if (console == null) throw new RuntimeException ("No console on this sytem, exiting!");
+			if (console.readLine("Enter \"s\" if you only want statistics\n").equals("s")) return;
 
-		if (console.readLine("Enter \"s\" if you only want statistics\n").equals("s")) return;
+			while (true) {
 
-		while (true) {
+				//reading data from console
+				String entry = console.readLine("Enter \"ml,percentAlcohol\" for today OR \"ml,percentAlcohol,Date(yyyymmdd)\"\nTo adjust DB entries use negative ml values\n\n");
+				String parts [] = entry.split(",");
 
-			String entry = console.readLine("Enter comma-separated \"ml,percentAlcohol for today or ml,percentAlcohol,Date formatted YYYYMMDD\"\n");
-			String parts [] = entry.split(",");
-                	float ml = (float) Double.parseDouble(parts[0]);
-                	float percent = (float) Double.parseDouble(parts[1]);
-			String yearWeek = null;
-			LocalDate date = null;
-			if (parts.length == 2) date = LocalDate.now(); else {DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyyMMdd"); date = LocalDate.parse(parts[2], f);}
+				//extracting individual parts of data, checking validity of input
+				float ml = 0;
+				float percent = 0;
+
+                		try {
+					ml = (float) Double.parseDouble(parts[0]);
+					percent = (float) Double.parseDouble(parts[1]);
+				}
+				catch (NumberFormatException|ArrayIndexOutOfBoundsException e) {System.out.println("\nenter at least 2 comma-separated numbers for ml and percent, bitch\n"); continue; }
+				//chacking validity associating the date (now or provided by user) to ISO week-based--year and week of week-based-year
+				String yearWeek = null;
+				LocalDate date = null;
+
+				if (parts.length == 2) date = LocalDate.now();
+				else {
+					try {
+						DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyyMMdd"); date = LocalDate.parse(parts[2], f);
+					} catch (DateTimeParseException d) {
+						System.out.println("\nenter a valid date of format yyyymmdd, bitch\n");
+						continue;
+					}
+				}
                                 
-			int week = date.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
-                        int weekYear = date.get(IsoFields.WEEK_BASED_YEAR);
-                        yearWeek = "" + weekYear + "_" + week;
+				int week = date.get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
+                        	int weekYear = date.get(IsoFields.WEEK_BASED_YEAR);
 
-			if (!console.readLine("You entered: ml: " + ml + ", percent: " + percent + ", WeekOfYear: " + yearWeek + ", OK to write to DB? (y/n)\n").equals("y")) continue;
+                        	yearWeek = "" + weekYear + "_" + week;
+
+				if (!console.readLine("You entered: ml: " + ml + ", percent: " + percent + ", ISOyear_week: " + yearWeek + ", OK to write to DB? (y/n)\n").equals("y")) break;
                 	
-			float realMl = ml * percent / 100;
 
-			if (thoseWeeks.containsKey("" + weekYear + "_" + week)) {
-				thoseWeeks.put("" + weekYear + "_" + week, realMl + thoseWeeks.get("" + weekYear + "_" + week));
-			} else thoseWeeks.put("" + weekYear + "_" + week, realMl);
+				//calculating ethyl ml
+				float realMl = ml * percent / 100;
 
-			if (console.readLine("One more drink? (y/n)\n").equals("y")) continue; else break;
-		}
+				if (thoseWeeks.containsKey("" + weekYear + "_" + week)) {
+					//adding up ethyl ml if year_week is already in the DB
+					thoseWeeks.put("" + weekYear + "_" + week, realMl + thoseWeeks.get("" + weekYear + "_" + week));
+				} else thoseWeeks.put("" + weekYear + "_" + week, realMl);
 
-}
+				if (console.readLine("One more drink? (y/n)\n").equals("y")) continue; else break;
+			}
 
-public void enterAndCalcItMock (String weekOfYear, Float mlAlcohol) {
-	thoseWeeks.put(weekOfYear, mlAlcohol);
-}
-
-public void printStats () {
-	thoseWeeks.forEach((k, v) -> System.out.println(k + " " + v));
-}
-
-public void serialize (Stats toBeSerialized) {
-	try {
-		ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(Paths.get("Stats.ser")));
-       		out.writeObject(toBeSerialized);
-       		out.close();
-	} catch (IOException i) {
-       		i.printStackTrace();
 	}
-}
+
+	public void enterAndCalcItMock (String weekOfYear, Float mlAlcohol) {
+		thoseWeeks.put(weekOfYear, mlAlcohol);
+	}
+
+	public void printStats () {
+		System.out.println("\nISOyear_week\tml of ethyl alcohol consumed\n");
+		thoseWeeks.forEach((k, v) -> System.out.println(k + "\t\t" + v));
+	}
+
+	public void serialize (Stats toBeSerialized) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(Paths.get("Stats.ser")));
+       			out.writeObject(toBeSerialized);
+       			out.close();
+		} catch (IOException i) {
+       			i.printStackTrace();
+		}
+	}
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # niceandeasy
+command to run the jar:
+java -jar NiceAndEasy.jar
+
+when run first time it will create Stats.ser in the working dir - that's the db, you can delete it if want to start from the scratch
